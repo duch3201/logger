e0014c456e0a563e8039ac81956341673c73cb4c
_Ghost_ <56601374+duch3201@users.noreply.github.com>
2022-08-11 00:50:21 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -2,6 +2,9 @@ import os
 import sys
 from datetime import date
 
+#made by duch3201 on github, this lib does not come with any warrenty,
+#if you wish to modify this code and re distribute it please give credit to og author https://github.com/duch3201
+
 def info(message):
     mes = "[I] " + str(date.today()) + " | " + message
     with open("logs.log", 'a') as logfile:
